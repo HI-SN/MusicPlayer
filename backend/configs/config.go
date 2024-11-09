2fce6af40b081a80bf44e2bbd6c76dbda378e004
HI-SN <131469059+HI-SN@users.noreply.github.com>
2024-11-09 21:17:56 +0800

    Add files via upload

--- backend/configs/config.go
+++ backend/configs/config.go
@@ -1,1 +1,37 @@
+package config
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
+
+type Config struct {
+	Database struct {
+		Type     string `json:"type"`
+		Host     string `json:"host"`
+		Port     int    `json:"port"`
+		Username string `json:"username"`
+		Password string `json:"password"`
+		Name     string `json:"name"`
+	} `json:"database"`
+	Server struct {
+		Port     string `json:"port"`
+		LogLevel string `json:"log_level"`
+	} `json:"server"`
+}
+
+var AppConfig Config
+
+// LoadConfig 从配置文件加载配置
+func LoadConfig(filePath string) {
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Fatalf("无法读取配置文件: %v", err)
+	}
+
+	err = json.Unmarshal(data, &AppConfig)
+	if err != nil {
+		log.Fatalf("无法解析配置文件: %v", err)
+	}
+}
