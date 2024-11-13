eba1e3fb44e39849a45eeae7999a7b5baced7b60
Dr_Tikas <1796654305@qq.com>
2024-11-13 19:03:30 +0800

    实现了歌手信息在数据库的增删改查

--- backend/models/artist.go
+++ backend/models/artist.go
@@ -1,5 +1,7 @@
 package models
 
+import "database/sql"
+
 type Artist struct {
 	Artist_id   int
 	Name        string
@@ -11,4 +13,40 @@ type Artist struct {
 
 func (Artist) TableName() string {
 	return "artist_info" // 数据库中的表名
+}
+
+// 创建歌手信息
+func CreateArtist(db *sql.DB, a *Artist) error {
+	query := `INSERT INTO artist_info (name, bio, profile_pic, type, nation) VALUES (?, ?, ?, ?, ?)`
+	_, err := db.Exec(query, a.Name, a.Bio, a.Profile_pic, a.Type, a.Nation)
+	return err
+}
+
+// 更新歌手信息
+func UpdateArtist(db *sql.DB, a *Artist) error {
+	query := `UPDATE artist_info SET name = ?, bio = ?, profile_pic = ?, 
+	type = ?, nation = ? WHERE id = ?`
+	_, err := db.Exec(query, a.Name, a.Bio, a.Profile_pic, a.Type, a.Nation, a.Artist_id)
+	return err
+}
+
+// 通过id获取歌手信息
+func GetArtist(db *sql.DB, artistID int) (*Artist, error) {
+	a := &Artist{}
+	query := "SELECT * FROM artist_info WHERE id=?"
+	err := db.QueryRow(query, artistID).Scan(&a.Artist_id, &a.Name, &a.Bio, &a.Profile_pic, &a.Type, &a.Nation)
+	if err != nil {
+		return nil, err
+	}
+	return a, nil
+}
+
+// 删除歌手信息
+func DeleteArtist(db *sql.DB, artistID int) error {
+	// 删除歌手的关联内容（专辑、歌曲等）
+
+	// 删除歌手
+	query := `DELETE FROM artist_info WHERE id = ?`
+	_, err := db.Exec(query, artistID)
+	return err
 }
